84a2fe7668cef6537d87674efe007cccb319aeb4
Robert Kleemann <robert@kleemann.org>
2024-01-15 19:00:59 -0800

    clarified comments

--- src/test/scala/org/kleemann/stocknotes/TestPrice.scala
+++ src/test/scala/org/kleemann/stocknotes/TestPrice.scala
@@ -21,17 +21,18 @@ class TestPrice extends munit.FunSuite {
         val d1 = p1.atMult(m1)
         assert(d1 >  9.99)
         assert(d1 < 10.01)
-        // 5:1 split, every five shares becomes 1, divide by five
+        // 5:1 split, every share becomes five, multiply shares by five.
+        // All things being equal, the price should divide by five
         val m2 = m1*Fraction(5,1)
         val d2 = p1.atMult(m2)
         assert(clue(d2) > 1.99)
         assert(d2 < 2.01)
-        // 2:1 split, every two shares becomes 1, divide by two
+        // 2:1 split, shares double, prices are cut in half
         val m3 = m2*Fraction(2,1)
         val d3 = p1.atMult(m3)
         assert(clue(d3) > 0.99)
         assert(d3 < 1.01)
-        // 1:3 split, every share becomes 3, multiplay by three
+        // a reverse 1:3 split, every share becomes 3, price multiplies by three
         val m4 = m3*Fraction(1,3)
         val d4 = p1.atMult(m4)
         assert(clue(d4) > 2.99)
