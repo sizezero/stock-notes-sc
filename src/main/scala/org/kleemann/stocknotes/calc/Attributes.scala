eee3a7a0ea4701984e161be36104a738069eeeac
Robert Kleemann <robert@kleemann.org>
2024-02-06 10:46:27 -0800

    split Attributes and Processor out of Calc

---
+++ src/main/scala/org/kleemann/stocknotes/calc/Attributes.scala
@@ -0,0 +1,35 @@
+package org.kleemann.stocknotes.calc
+
+import org.kleemann.stocknotes.stock.Currency
+
+/**
+ * This is the list of all possible attributes both parsed and generated.
+ * Any attribute may be missing.
+ *
+ * @param income annual income in dollars
+ * @param revenue annual revenue in dollars
+ * @param shares share count, hopefully fully diluted
+ * @param eps earnings per share in dollars
+ * @param pe price earnings multiple
+ * @param marketCap total market capitalization in dollars
+ * @param price price per share in dollars
+ * @param margins net margins: income/revenue
+ * @param dividend annual dividend per share
+ * @param dividendYield annual dividend yield
+ * @param payoutRatio dividend / income
+ * @param help not really an attribute, just outputs the help for all the above attributes
+ */
+private final case class Attributes(
+        income:        Option[Currency] = None,
+        revenue:       Option[Currency] = None,
+        shares:        Option[Int]      = None,
+        eps:           Option[Currency] = None,
+        pe:            Option[Double]   = None,
+        marketCap:     Option[Currency] = None,
+        price:         Option[Currency] = None,
+        margins:       Option[Double]   = None,
+        dividend:      Option[Currency] = None,
+        dividendYield: Option[Double]   = None,
+        payoutRatio:   Option[Double]   = None,
+        help:          Option[Boolean]  = None
+    )
