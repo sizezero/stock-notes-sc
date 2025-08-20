3ad5bd2a1440872e9eb3b7d94f042c99a2250dfd
Robert Kleemann <robert@kleemann.org>
2025-08-20 15:11:33 -0700

    refactor

--- src/main/scala/org/kleemann/stocknotes/current/StockReport.scala
+++ src/main/scala/org/kleemann/stocknotes/current/StockReport.scala
@@ -236,14 +236,14 @@ object StockReport {
 
       // percentage of shares that are ltcg vs short
       val m = Fraction.one // all calculations need to be at the same multiple but it doesn't matter which one
-      val numerator: Double = mss.foldLeft(0.0){ (acc, ms) =>
+      val ltcgShares: Double = mss.foldLeft(0.0){ (acc, ms) =>
         acc + ms.mbs.foldLeft(0.0){ (acc, mb) => 
           acc + (if (mb.ltcg) mb.sold.atMult(m) else 0.0)
         } 
       }
-      val denominator: Double = mss.foldLeft(0.0){ (acc, ms) => acc + ms.sell.shares.atMult(m) }
-      val ltcgPercentage = numerator / denominator
-        
+      val totalShares: Double = mss.foldLeft(0.0){ (acc, ms) => acc + ms.sell.shares.atMult(m) }
+      val ltcgPercentage = ltcgShares / totalShares
+
       Some(StockReport(stock, mss, net.truncate, capGains.truncate, ltcgPercentage))
     }
   }
