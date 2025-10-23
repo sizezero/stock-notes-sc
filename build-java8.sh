531d2fbe26d7259a018747812f3bbfb14d9ec026
Robert Kleemann <robert@kleemann.org>
2025-10-23 10:55:14 -0700

    verify java8 is installed before building

--- build-java8.sh
+++ build-java8.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
+
+if [[ ! -d $JAVA_HOME ]]; then
+    echo "java 8 needs to be installed"
+    echo "$ sudo apt install openjdk-8-jdk"
+    exit 1
+fi
+
 PATH=$JAVA_HOME/bin:$PATH
 sbt clean
 sbt compile
